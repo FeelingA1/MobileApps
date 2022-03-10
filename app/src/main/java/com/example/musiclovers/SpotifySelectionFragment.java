26f2324d98d6a3a0d2fc26fbdfe6985dec67ee6c
Elijah Yates <yates.281@buckeyemail.osu.edu>
2022-03-10 13:20:57 -0500

    Added the first skeleton of the classes

---
+++ app/src/main/java/com/example/musiclovers/SpotifySelectionFragment.java
@@ -0,0 +1,77 @@
+package com.example.musiclovers;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musiclovers.databinding.FragmentSpotifySelectionBinding;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class SpotifySelectionFragment extends Fragment{
+
+    private FragmentSpotifySelectionBinding binding;
+    private String TAG = "PostSelectionFragment";
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        Log.d(TAG, "onCreateView");
+        binding = FragmentSpotifySelectionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Will have multiple setOnclickListeners to handle transitioning to other fragements or activities
+
+        //Need to update the button so it isn't buttonFirst but maybe buttonPost
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Must change the .nagigate to different fragments. needs to be a different id
+                NavHostFragment.findNavController(SpotifySelectionFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
