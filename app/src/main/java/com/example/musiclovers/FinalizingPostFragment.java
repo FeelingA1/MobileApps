18cccf8847974a862b07eef21d52145da2dbecda
Robert Lawrence <robertlawrence14610@gmail.com>
2022-04-10 18:35:00 -0400

    added connection to Firebase when completing post within post fragment

--- app/src/main/java/com/example/musiclovers/FinalizingPostFragment.java
+++ app/src/main/java/com/example/musiclovers/FinalizingPostFragment.java
@@ -5,19 +5,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.musiclovers.databinding.FragmentFinalizingPostBinding;
 import com.example.musiclovers.databinding.FragmentSpotifySelectionBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FinalizingPostFragment extends Fragment{
 
     //Thought this would get us to the next fragment, but maybe not right. Might want activity
     private FragmentFinalizingPostBinding binding;
     private String TAG = "FinalizingPostFragment";
+    private EditText mPostContentEditText;
+    private EditText mLinkEditText;
+
+    // Initialize Cloud Firestore
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     public View onCreateView(
@@ -26,8 +39,8 @@ public class FinalizingPostFragment extends Fragment{
     ) {
         Log.d(TAG, "onCreateView");
         binding = FragmentFinalizingPostBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        return binding.getRoot();
     }
 
     @Override
@@ -57,15 +70,21 @@ public class FinalizingPostFragment extends Fragment{
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //Will have multiple setOnclickListeners to handle transitioning to other fragements or activities
+        mLinkEditText = (EditText) view.findViewById(R.id.LinkTextBox);
+        mPostContentEditText = (EditText) view.findViewById(R.id.ThoughtsTextBox);
+
+        //Will have multiple setOnclickListeners to handle transitioning to other fragments or activities
 
         //Need to update the button so it isn't buttonFirst but maybe buttonPost
         binding.buttonPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Must change the .nagigate to different fragments. needs to be a different id
+                addPost();
+
+                //Must change the .navigate to different fragments. needs to be a different id
                 NavHostFragment.findNavController(FinalizingPostFragment.this)
                         .navigate(R.id.action_FinalizingPost_to_Main);
+
             }
         });
     }
@@ -76,4 +95,27 @@ public class FinalizingPostFragment extends Fragment{
         binding = null;
     }
 
+    // Add post to the database
+    public void addPost() {
+        Map<String, Object> post = new HashMap<>();
+        post.put("userID", "test");
+        post.put("text", mPostContentEditText.getText().toString());
+        post.put("link", mLinkEditText.getText().toString());
+
+        // Add document with generated ID
+        db.collection("Posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
 }
